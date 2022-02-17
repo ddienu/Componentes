e446a90934facef318036988774ef293cca6513b
ddienu <rockdann@hotmail.com>
2022-02-17 00:05:01 -0500

    Add: Routes page.
    Add the routes page for better organization of the program.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';   
-import 'package:componentes/src/pages/home_page.dart';
+
 import 'package:componentes/src/pages/alert_page.dart';
-import 'package:componentes/src/pages/avatar_page.dart';
+
+import 'package:componentes/src/routes/routes.dart';
 //import 'package:componentes/src/pages/home_temp.dart';
 
 void main() => runApp(MyApp());
@@ -14,11 +15,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       // home: HomePage()
       initialRoute: '/',
-      routes: <String, WidgetBuilder>{
-        '/'      : (BuildContext context) => HomePage(),
-        'alert'  : (BuildContext context) => AlertPage(),
-        'avatar' : (BuildContext context) => AvatarPage(),        
-      }, 
+      routes: getAplicattionRoutes()
     );
   }
 }
