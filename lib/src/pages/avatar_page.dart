5d9d73204fb28dab1f1f3d11917fd2b5ac733a1c
ddienu <rockdann@hotmail.com>
2022-03-16 23:45:08 -0500

    add: Avatar's page characteristics, add: Containers and avatars.
    Create the description of the avatar page, including two new avatars and network images.

--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -10,6 +10,32 @@ class AvatarPage extends StatelessWidget {
 
       appBar: AppBar(
         title: Text ('AvatarPage'),
+        actions: [
+
+          Container(
+            padding: EdgeInsets.all(5.0),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage('https://img.olympicchannel.com/images/image/private/t_1-1_600/f_auto/v1538355600/primary/fzlkiegp1vzcpvrtm1m7'),
+              radius: 30.0,
+            ),
+          ),
+
+          Container(
+            margin: EdgeInsets.only( right: 10.0),
+            child: CircleAvatar(
+              child: Text('ML'),
+              backgroundColor: Colors.amber,
+
+            ),
+          )
+        ],
+      ),
+      body: Center(
+        child: FadeInImage(
+          image: NetworkImage('https://s3.amazonaws.com/arc-wordpress-client-uploads/infobae-wp/wp-content/uploads/2016/07/30111616/ma-long-1920-2.jpg'),
+          placeholder: AssetImage('Assets/jar-loading.gif'),
+          fadeInDuration: Duration( milliseconds: 200),
+        ),
       ),
     );
 
