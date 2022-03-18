9121201928d1328a374f7e7f03b474c268f0bdc1
ddienu <rockdann@hotmail.com>
2022-03-17 23:00:32 -0500

    Add: Animated Container Page, add: Characteristics Animated's Page,
    Add the characteristics of the animated's page, including animated container with curves to enhance the visual effect in the shape.

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:componentes/src/pages/alert_page.dart';
 import 'package:componentes/src/pages/avatar_page.dart';
 import 'package:componentes/src/pages/home_page.dart';
+import 'package:componentes/src/pages/animated_container.dart';
 
 Map<String, WidgetBuilder> getAplicattionRoutes(){
 
@@ -12,5 +13,6 @@ Map<String, WidgetBuilder> getAplicattionRoutes(){
         'alert'  : (BuildContext context) => AlertPage(),
         'avatar' : (BuildContext context) => AvatarPage(), 
         'card'   : (BuildContext context) => CardPage(),   
+        'animatedContainer'   : (BuildContext context) => AnimatedContainerPage(),  
       };
 } 
