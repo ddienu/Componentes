94e4a8c8f2cf29cc52f903b781638d62b7c09fe3
ddienu <rockdann@hotmail.com>
2022-03-29 23:37:16 -0500

    add: Slider page.
    Add the slider page, using the slider option and one image which can be management its width with the slider.

---
+++ lib/src/pages/slider_page.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+
+
+class SliderPage extends StatefulWidget {
+  SliderPage({Key? key}) : super(key: key);
+
+  @override
+  State<SliderPage> createState() => _SliderPageState();
+}
+
+class _SliderPageState extends State<SliderPage> {
+
+double _valorSlider = 100.0;
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text ('Slider'),
+      ),
+      body: Container(
+        padding: EdgeInsets.only( top: 50.0),
+        child: Column(
+          children: [
+            _crearSlider(),
+            Expanded (child: _crearImagen()),
+          ],
+        ),
+      ),
+    );
+  }
+
+  _crearSlider() {
+
+    return Slider(
+      activeColor: Colors.orangeAccent,
+      label: 'Tamaño de la imágen',
+      // divisions: 20,
+      value: _valorSlider,
+      min: 10.0,
+      max: 400.0,
+      onChanged: ( valor ){
+
+        setState(() {
+        _valorSlider = valor;
+        });
+      }
+      );
+  }
+
+  _crearImagen() {
+
+    return Image(
+     image: NetworkImage('https://img2.freepng.es/20191009/owi/transparent-orange-5d9de576bcfb39.4355972915706289827741.jpg'),
+     fit: BoxFit.contain,
+     width: _valorSlider,
+    );
+
+  }
+} 
+          
--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -1,21 +1,23 @@
-import 'package:componentes/src/pages/card_page.dart';
 import 'package:flutter/material.dart';
 
 import 'package:componentes/src/pages/alert_page.dart';
 import 'package:componentes/src/pages/avatar_page.dart';
 import 'package:componentes/src/pages/home_page.dart';
+import 'package:componentes/src/pages/card_page.dart';
 import 'package:componentes/src/pages/animated_container.dart';
 import 'package:componentes/src/pages/input_page.dart';
+import 'package:componentes/src/pages/slider_page.dart';
 
 Map<String, WidgetBuilder> getAplicattionRoutes(){
 
  return <String, WidgetBuilder>{
-        '/'      : (BuildContext context) => HomePage(),
-        'alert'  : (BuildContext context) => AlertPage(),
-        'avatar' : (BuildContext context) => AvatarPage(), 
-        'card'   : (BuildContext context) => CardPage(),   
+        '/'        : (BuildContext context) => HomePage(),
+        'alert'    : (BuildContext context) => AlertPage(),
+        'avatar'   : (BuildContext context) => AvatarPage(), 
+        'card'     : (BuildContext context) => CardPage(),   
         'animatedContainer'   : (BuildContext context) => AnimatedContainerPage(),  
-        'inputs'   : (BuildContext context) => InputPage(),  
+        'inputs'   : (BuildContext context) => InputPage(), 
+        'slider'   : (BuildContext context) => SliderPage(),   
       
 
       };
