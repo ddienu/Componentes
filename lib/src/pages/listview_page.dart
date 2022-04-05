0b5630238aae6f4abb309d99e82b3ab283f00959
ddienu <rockdann@hotmail.com>
2022-04-04 23:40:35 -0500

    Add: InfiniteScroll
    Add infinite scroll in the list view page, every time the scroll gets the amount of 10 pictures, the scroll load another 10 images.

--- lib/src/pages/listview_page.dart
+++ lib/src/pages/listview_page.dart
@@ -10,7 +10,23 @@ class ListaPage extends StatefulWidget {
 
 class _ListaPageState extends State<ListaPage> {
 
-  List<int> _listaNumeros = [1,2,3,4,5];
+  ScrollController _scrollController = new ScrollController();
+
+  final List<int> _listaNumeros = [];
+  int _ultimoItem = 0;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _agregar10();
+
+    _scrollController.addListener(() { 
+
+      if (_scrollController.position.pixels == _scrollController.position.maxScrollExtent);
+      _agregar10();
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,17 +40,29 @@ class _ListaPageState extends State<ListaPage> {
   _crearLista() {
 
     return ListView.builder(
+      controller: _scrollController,
       itemCount: _listaNumeros.length,
       itemBuilder: (BuildContext context, int index){
 
         final imagen = _listaNumeros[index];
 
         return FadeInImage(
-          image: NetworkImage('https://picsum.photos/200/300?image=$imagen'),
+          image: NetworkImage('https://picsum.photos/500/300/?image=$imagen'),
           placeholder: AssetImage('Assets/jar-loading.gif')
           
           );
       },
     );
+    }
+
+  void _agregar10() {
+
+    for (var i = 1; i < 10; i++) {
+       _ultimoItem++;
+       _listaNumeros.add(_ultimoItem);
+     }
+     setState(() {
+       
+     });
   }
 }
