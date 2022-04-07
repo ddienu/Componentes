77b19fc88d3b7437f011ee8ab875057f78a3fc38
ddienu <rockdann@hotmail.com>
2022-04-06 23:30:31 -0500

    Add: Refresh indicator.
    Add pull to refresh which load 10 new images.

--- lib/src/pages/listview_page.dart
+++ lib/src/pages/listview_page.dart
@@ -57,20 +57,39 @@ class _ListaPageState extends State<ListaPage> {
 
   _crearLista() {
 
-    return ListView.builder(
-      controller: _scrollController,
-      itemCount: _listaNumeros.length,
-      itemBuilder: (BuildContext context, int index){
-
-        final imagen = _listaNumeros[ index ];
-
-        return FadeInImage(
-          image: NetworkImage('https://picsum.photos/500/300/?image=$imagen'),
-          placeholder: AssetImage('Assets/jar-loading.gif')
-          
-          );
-      },
+    return RefreshIndicator(
+        onRefresh: obtenerPagina1,
+        child: ListView.builder(
+        controller: _scrollController,
+        itemCount: _listaNumeros.length,
+        itemBuilder: (BuildContext context, int index){
+    
+          final imagen = _listaNumeros[ index ];
+    
+          return FadeInImage(
+            image: NetworkImage('https://picsum.photos/500/300/?image=$imagen'),
+            placeholder: AssetImage('Assets/jar-loading.gif')
+            
+            );
+        },
+      ),
     );
+    }
+
+    Future<Null> obtenerPagina1() async{
+
+      final duration = Duration( seconds: 2);
+      
+      new Timer(duration, (){
+
+        _listaNumeros.clear();
+        _ultimoItem++;
+        _agregar10();
+      });
+
+      return Future.delayed(duration);
+
+
     }
 
   void _agregar10() {
